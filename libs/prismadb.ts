26ac6aafc8865fd05d3f8621ee5f2713f8721707
onose <ogbidionose@gmail.com>
2026-02-08 23:09:09 +0100

    fix: prismadb file error

--- libs/prismadb.ts
+++ libs/prismadb.ts
@@ -1,17 +1,11 @@
-// import { PrismaClient } from "@prisma/client";
-
-// declare global {
-//     var prisma: PrismaClient | undefined
-// }
-
-// const prisma = global.prisma || new PrismaClient({log: ["error", "warn"]});
-
-// if(process.env.NODE_ENV !== 'production') global.prisma = prisma
-
-// export default prisma
-
 import { PrismaClient } from "@prisma/client";
 
-const prisma = new PrismaClient();
+declare global {
+    var prisma: PrismaClient | undefined
+}
 
-export default prisma;
+const prisma = global.prisma || new PrismaClient({log: ["error", "warn"]});
+
+if(process.env.NODE_ENV !== 'production') global.prisma = prisma
+
+export default prisma
